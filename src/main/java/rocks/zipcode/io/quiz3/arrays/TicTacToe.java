4ac271b97a9252ee76e293d536e485c277e1dde4
k-romero <imkevinromero@gmail.com>
2020-03-09 10:34:42 -0400

    checkpoint 2

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -4,34 +4,105 @@ package rocks.zipcode.io.quiz3.arrays;
  * @author leon on 09/12/2018.
  */
 public class TicTacToe {
+    String[][] board;
 
     public TicTacToe(String[][] board) {
+        this.board = board;
     }
 
     public TicTacToe() {
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        String[] result = new String[3];
+        for (int i = 0; i < 3; i++) {
+            result[i] = board[value][i];
+        }
+        return result;
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        String[] result = new String[3];
+        for (int i = 0; i < 3; i++) {
+            result[i] = board[i][value];
+        }
+        return result;
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
-        return null;
+        if (getRow(rowIndex)[0] == getRow(rowIndex)[1]
+                && getRow(rowIndex)[1] == getRow(rowIndex)[2]) {
+            return true;
+        }
+
+        return false;
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
-        return null;
+        if (getColumn(columnIndex)[0] == getColumn(columnIndex)[1]
+                && getColumn(columnIndex)[1] == getColumn(columnIndex)[2]) {
+            return true;
+        }
+
+        return false;
     }
 
     public String getWinner() {
-        return null;
+        String winner = "";
+        for (int i = 0; i < 3; i++) {
+            if(isColumnHomogeneous(i)){
+                winner = getColumn(i)[0];
+                break;
+            }
+            else if(isRowHomogenous(i)){
+                winner = getRow(i)[0];
+                break;
+            }
+        }
+
+        if(topLeftBotRightCheck("X")){
+            winner = "X";
+        } else if(topRightBotLeftCheck("X")){
+            winner = "X";
+        } else if(topLeftBotRightCheck("O")){
+            winner = "O";
+        } else if(topRightBotLeftCheck("O")){
+            winner = "O";
+        }
+
+        return winner;
     }
 
     public String[][] getBoard() {
         return null;
+    }
+
+    public boolean topLeftBotRightCheck(String playerPiece) {
+        boolean outcome = false;
+        for (int i = 0; i <= 2; i++) {
+            if (!board[i][i].equals(playerPiece)) {
+                outcome = false;
+                break;
+            } else {
+                outcome = true;
+            }
+        }
+        return outcome;
+    }
+
+    //    Diagonal (/)
+    public boolean topRightBotLeftCheck(String playerPiece) {
+        boolean outcome = false;
+        int count = 0;
+        for (int i = 2; i >= 0; i--) {
+            if (!board[count][i].equals(playerPiece)) {
+                outcome = false;
+                break;
+            } else {
+                outcome = true;
+                count++;
+            }
+        }
+        return outcome;
     }
 }
