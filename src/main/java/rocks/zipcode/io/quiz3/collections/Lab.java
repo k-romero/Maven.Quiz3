73b254ca448b2f1243fd445521d887add4a33e8b
k-romero <imkevinromero@gmail.com>
2020-03-09 12:36:44 -0400

    mid way

--- src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
@@ -4,21 +4,32 @@ package rocks.zipcode.io.quiz3.collections;
  * @author leon on 10/12/2018.
  */
 public class Lab {
+    private LabStatus labStatus;
+    private String labName;
+
     public Lab() {
         this(null);
+        this.labStatus = LabStatus.PENDING;
     }
 
     public Lab(String labName) {
+        this.labName = labName;
     }
 
     public String getName() {
-        return null;
+        return labName;
     }
 
     public void setStatus(LabStatus labStatus) {
+        if(labStatus == LabStatus.COMPLETED){
+            this.labStatus = LabStatus.COMPLETED;
+        } else if(labStatus == LabStatus.INCOMPLETE){
+//            this.labStatus = LabStatus.
+        }
+
     }
 
     public LabStatus getStatus() {
-        return null;
+        return labStatus;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/collections/LabStatus.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/LabStatus.java
@@ -5,5 +5,10 @@ package rocks.zipcode.io.quiz3.collections;
  * Ensure there are enumerations for, `COMPLETED`, `INCOMPLETE`, `PENDING`
  */
 public enum LabStatus {
-    ADD_ENUMERATIONS_HERE;
+    ADD_ENUMERATIONS_HERE,
+    COMPLETED,
+    INCOMPLETE,
+    PENDING;
+
+
 }
